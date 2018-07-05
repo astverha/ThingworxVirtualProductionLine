a9170c3fe5ca1714990c667571c8b6670c6f3aa9
ddvlanck <dwight.vanlancker@hotmail.com>
2018-07-05 10:42:20 +0200

    GuiThread created

--- Linev2/src/main/java/com/stage/client/ThreadManager.java
+++ Linev2/src/main/java/com/stage/client/ThreadManager.java
@@ -2,73 +2,92 @@ package com.stage.client;
 
 import configuration.AssetThing;
 import configuration.ConfigurationAgent;
+import gui.GUIAgent;
+import gui.VirtualProductLine;
 import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.LoggerFactory;
 
 public class ThreadManager {
-    
+
     private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(ThreadManager.class);
-    
+
     private final List<Thread> threads;
     private final ConfigurationAgent agent;
     private final ThingworxClient client;
+    private final Thread GUIThread;
     private boolean pauseThread;
-    
-    public ThreadManager(ConfigurationAgent agent){
+
+    public ThreadManager(ConfigurationAgent agent) {
         this.agent = agent;
         this.client = agent.getClient();
         threads = new ArrayList<>();
+        this.GUIThread = new Thread(new GUIThreadRunnable());
         pauseThread = false;
     }
-    
+
     //Start threads
-    public void start(){
+    public void start() {
         try {
-            if(client.waitForConnection(30000)){
-                for(AssetThing thing : agent.getThings()){    
-                    if(!thing.getName().contains("Line")){
+            //Start GUI
+            GUIThread.start();
+            
+            //Start simulation
+            if (client.waitForConnection(30000)) {
+                for (AssetThing thing : agent.getThings()) {
+                    if (!thing.getName().contains("Line")) {
                         Thread thingThread = new Thread(new AgentThreadRunnable(thing, 5));
                     }
                 }
             }
-        } catch(Exception e){
+        } catch (Exception e) {
             LOG.error("NOTIFICATIE [ERROR] - {} - An exception occurred while starting the threads.", ThreadManager.class);
         }
     }
-    
+
     private class AgentThreadRunnable implements Runnable {
 
         private final AssetThing thing;
         private final int speed;
-        
+
         private AgentThreadRunnable(AssetThing thing, int speed) {
             this.speed = speed;
             this.thing = thing;
         }
-        
+
         @Override
         public void run() {
-            while(!client.isShutdown()){
-                if(pauseThread){
+            while (!client.isShutdown()) {
+                if (pauseThread) {
                     //TO DO
                 } else {
                     try {
                         client.bindThing(this.thing);
-                        if(client.isConnected()){
+                        if (client.isConnected()) {
                             try {
                                 this.thing.simulateData();
-                            } catch(Exception e){
+                            } catch (Exception e) {
                                 LOG.error("NOTIFICATIE [ERROR] - {} - Exception occurred while simulating new data.", AgentThreadRunnable.class);
                             }
                         }
-                                
-                    } catch(Exception e){
+
+                    } catch (Exception e) {
                         LOG.error("NOTIFICATIE [ERROR] - {} - Thing {} could not be bound to the client.", AgentThreadRunnable.class, this.thing.getName());
                     }
                 }
             }
         }
-        
+
+    }
+
+    private class GUIThreadRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            java.awt.EventQueue.invokeLater(() -> {
+                new VirtualProductLine(new GUIAgent(agent)).setVisible(true);
+            });
+        }
+
     }
 }
